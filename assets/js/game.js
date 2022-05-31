88e00ebd552fe7dab79baf0f697bbebe0290208a
unknown <misterjimlee@gmail.com>
2022-05-31 18:10:27 -0400

    console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("Captain's Log:");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
 window.alert("This is an alert! JavaScript is running!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
